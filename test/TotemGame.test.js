8e12e2b88bfe4850f1633a3e678c51b790835c4b
starman69 <dave@pattens.org>
2025-01-29 15:55:19 -0800

    update tests, custom errors

--- test/TotemGame.test.js
+++ test/TotemGame.test.js
@@ -9,12 +9,7 @@ describe("TotemGame", function () {
     // Initial game parameters
     const gameParams = {
         signupReward: ethers.parseUnits("2000", 18),    // 2000 TOTEM
-        mintPrice: ethers.parseUnits("500", 18),        // 500 TOTEM
-        feedCost: ethers.parseUnits("10", 18),          // 10 TOTEM
-        trainCost: ethers.parseUnits("20", 18),         // 20 TOTEM
-        feedHappinessIncrease: 10n,
-        trainExperienceIncrease: 50n,
-        trainHappinessDecrease: 10n
+        mintPrice: ethers.parseUnits("500", 18)         // 500 TOTEM
     };
 
     // Time windows for feeding (in seconds from start of day UTC)
@@ -22,6 +17,43 @@ describe("TotemGame", function () {
         window1Start: 0n,      // 00:00 UTC
         window2Start: 28800n,  // 08:00 UTC
         window3Start: 57600n   // 16:00 UTC
+    };
+
+    // Action configurations
+    const actionConfigs = {
+        feed: {
+            cost: ethers.parseUnits("10", 18),      // 10 TOTEM
+            cooldown: 0n,
+            maxDaily: 3n,
+            minHappiness: 0n,
+            happinessChange: 10n,
+            experienceGain: 0n,
+            useTimeWindows: true,
+            increasesHappiness: true,
+            enabled: true
+        },
+        train: {
+            cost: ethers.parseUnits("20", 18),      // 20 TOTEM
+            cooldown: 0n,
+            maxDaily: 0n,
+            minHappiness: 20n,
+            happinessChange: 10n,
+            experienceGain: 50n,
+            useTimeWindows: false,
+            increasesHappiness: false,
+            enabled: true
+        },
+        treat: {
+            cost: ethers.parseUnits("20", 18),      // 20 TOTEM
+            cooldown: 14400n,
+            maxDaily: 2n,
+            minHappiness: 0n,
+            happinessChange: 10n,
+            experienceGain: 0n,
+            useTimeWindows: false,
+            increasesHappiness: true,
+            enabled: true
+        }
     };
 
     beforeEach(async function () {
@@ -53,20 +85,15 @@ describe("TotemGame", function () {
             await token.getAddress(),
             await nft.getAddress(),
             trustedForwarder.address,
-            [
-                gameParams.signupReward,
-                gameParams.mintPrice,
-                gameParams.feedCost,
-                gameParams.trainCost,
-                gameParams.feedHappinessIncrease,
-                gameParams.trainExperienceIncrease,
-                gameParams.trainHappinessDecrease
-            ],
-            [
-                timeWindows.window1Start,
-                timeWindows.window2Start,
-                timeWindows.window3Start
-            ]
+            {
+                signupReward: gameParams.signupReward,
+                mintPrice: gameParams.mintPrice
+            },
+            {
+                window1Start: timeWindows.window1Start,
+                window2Start: timeWindows.window2Start,
+                window3Start: timeWindows.window3Start
+            }
         ]);
 
         // 7. Deploy Proxy
@@ -97,11 +124,6 @@ describe("TotemGame", function () {
             const params = await game.gameParams();
             expect(params.signupReward).to.equal(gameParams.signupReward);
             expect(params.mintPrice).to.equal(gameParams.mintPrice);
-            expect(params.feedCost).to.equal(gameParams.feedCost);
-            expect(params.trainCost).to.equal(gameParams.trainCost);
-            expect(params.feedHappinessIncrease).to.equal(gameParams.feedHappinessIncrease);
-            expect(params.trainExperienceIncrease).to.equal(gameParams.trainExperienceIncrease);
-            expect(params.trainHappinessDecrease).to.equal(gameParams.trainHappinessDecrease);
         });
 
         it("Should have correct time windows set", async function () {
@@ -115,6 +137,42 @@ describe("TotemGame", function () {
             const gameplayTokens = (ethers.parseUnits("1000000000", 18) * 25n) / 100n; // 25% of 1 billion
             expect(await token.balanceOf(await game.getAddress())).to.equal(gameplayTokens);
         });
+
+        it("Should have correct action configurations", async function () {
+            const { params, windows, configs } = await game.getGameConfiguration();
+
+            // Verify game parameters
+            expect(params.signupReward).to.equal(gameParams.signupReward);
+            expect(params.mintPrice).to.equal(gameParams.mintPrice);
+
+            // Verify action configurations
+            const actionTypes = ['Feed', 'Train', 'Treat'];
+            actionTypes.forEach((actionType, index) => {
+                const actionConfig = configs[index];
+                let expectedConfig;
+                switch(actionType) {
+                    case 'Feed':
+                        expectedConfig = actionConfigs.feed;
+                        break;
+                    case 'Train':
+                        expectedConfig = actionConfigs.train;
+                        break;
+                    case 'Treat':
+                        expectedConfig = actionConfigs.treat;
+                        break;
+                }
+
+                expect(actionConfig.cost).to.equal(expectedConfig.cost);
+                expect(actionConfig.cooldown).to.equal(expectedConfig.cooldown);
+                expect(actionConfig.maxDaily).to.equal(expectedConfig.maxDaily);
+                expect(actionConfig.minHappiness).to.equal(expectedConfig.minHappiness);
+                expect(actionConfig.happinessChange).to.equal(expectedConfig.happinessChange);
+                expect(actionConfig.experienceGain).to.equal(expectedConfig.experienceGain);
+                expect(actionConfig.useTimeWindows).to.equal(expectedConfig.useTimeWindows);
+                expect(actionConfig.increasesHappiness).to.equal(expectedConfig.increasesHappiness);
+                expect(actionConfig.enabled).to.equal(expectedConfig.enabled);
+            });
+        });
     });
 
     describe("User Signup", function () {
@@ -127,7 +185,7 @@ describe("TotemGame", function () {
         it("Should prevent double signup", async function () {
             await game.connect(addr1).signup();
             await expect(game.connect(addr1).signup())
-                .to.be.revertedWith("Already signed up");
+                .to.be.revertedWithCustomError(game, "AlreadySignedUp");
         });
     });
 
@@ -148,12 +206,12 @@ describe("TotemGame", function () {
         it("Should fail buying tokens without signup", async function () {
             const polAmount = ethers.parseEther("1");
             await expect(game.connect(addr2).buyTokens({ value: polAmount }))
-                .to.be.revertedWith("Must sign up first");
+                .to.be.revertedWithCustomError(game, "NotSignedUp");
         });
 
         it("Should fail buying tokens with zero POL", async function () {
             await expect(game.connect(addr1).buyTokens({ value: 0 }))
-                .to.be.revertedWith("Must send POL");
+                .to.be.revertedWithCustomError(game, "NoPolSent");
         });
     });
 
@@ -176,18 +234,43 @@ describe("TotemGame", function () {
         it("Should fail to purchase with invalid species", async function () {
             const invalidSpeciesId = 255;
             await expect(game.connect(addr1).purchaseTotem(invalidSpeciesId))
-                .to.be.revertedWith("Invalid species");
+                .to.be.revertedWithCustomError(nft, "InvalidSpecies");
         });
 
         it("Should fail to purchase without signup", async function () {
             await expect(game.connect(addr2).purchaseTotem(0))
-                .to.be.revertedWith("Must sign up first");
+                .to.be.revertedWithCustomError(game, "NotSignedUp");
         });
 
         it("Should fail to purchase without sufficient token approval", async function () {
             await token.connect(addr1).approve(await game.getAddress(), 0); // Reset approval
             await expect(game.connect(addr1).purchaseTotem(0))
                 .to.be.revertedWithCustomError(token, "ERC20InsufficientAllowance");
+        });
+    });
+
+    describe("Action Execution", function () {
+        let tokenId;
+        beforeEach(async function () {
+            // Signup, purchase totem, and approve tokens
+            await game.connect(addr1).signup();
+            await token.connect(addr1).approve(await game.getAddress(), ethers.parseUnits("1000", 18));
+            
+            // Purchase a totem
+            await game.connect(addr1).purchaseTotem(0);
+            tokenId = await nft.tokenOfOwnerByIndex(addr1.address, 0);
+        });
+
+        it("Should allow feeding a totem", async function () {
+            await game.connect(addr1).feed(tokenId);
+        });
+
+        it("Should allow training a totem", async function () {
+            await game.connect(addr1).train(tokenId);
+        });
+
+        it("Should allow treating a totem", async function () {
+            await game.connect(addr1).treat(tokenId);
         });
     });
 });
