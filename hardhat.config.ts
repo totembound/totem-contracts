758b9799b562715a8d655ba5cc543734a66233a8
starman69 <dave@pattens.org>
2025-04-06 18:21:14 -0700

    update hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -42,8 +42,8 @@ const config: HardhatUserConfig = {
       chainId: 31337
     },
     amoy: {
-      url: process.env.POLYGON_AMOY_RPC_URL,
-      accounts: [process.env.PRIVATE_KEY!],
+      url: process.env.POLYGON_AMOY_RPC_URL || 'https://rpc-amoy.polygon.technology',
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
       chainId: 80002
     }
   },
