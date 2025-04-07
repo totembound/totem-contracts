bad33872aa0f4bd165b5d628b2822e7ad6d45b35
starman69 <dave@pattens.org>
2025-04-06 18:09:43 -0700

    update chainlink lib

--- contracts/oracles/TotemChainLinkPriceOracle.sol
+++ contracts/oracles/TotemChainLinkPriceOracle.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
-import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import { ITotemPriceOracle } from "../interfaces/ITotemPriceOracle.sol";
 
 contract TotemChainlinkPriceOracle is ITotemPriceOracle, Ownable {
--- contracts/oracles/TotemRandomOracle.sol
+++ contracts/oracles/TotemRandomOracle.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 import { VRFConsumerBaseV2 } from "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
-import { VRFCoordinatorV2Interface } from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import { VRFCoordinatorV2Interface } from "@chainlink/contracts/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
 import { ConfirmedOwner } from "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
 import { ITotemRandomOracle } from "../interfaces/ITotemRandomOracle.sol";
 
